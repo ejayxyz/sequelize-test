2e136104dabaec84f15011cf3ea927999c9a91e4
Martin Dezsi <dezsimartin@gmail.com>
2019-07-08 16:29:50 +0200

    db table

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const { table } = require('table');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mysql://tasi:tasi@192.168.5.123/zeta_test');
-console.log(sequelize);
+const sequelize = new Sequelize('mysql://root:dmArtini1994@localhost/zeta_test');
+// console.log(sequelize);
+
 sequelize
   .authenticate()
   .then(() => {
     sequelize.query('select * from games').then(result => {
-      console.log(result);
+      let row = result[0];
+      let matrix = [];
+      for (let i = 0; i < row.length; i++) {
+        matrix.push([
+          row[i].id,
+          row[i].name,
+          row[i].description
+        ]);
+      }
+      console.log(table(matrix));
     });
   })
   .catch(err => {
