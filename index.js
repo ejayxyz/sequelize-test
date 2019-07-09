4aa039992f887bf96f5fd853146e90adaf488e86
Martin Dezsi <dezsimartin@gmail.com>
2019-07-09 23:02:08 +0200

    seq modified.

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 const { table } = require('table');
 let readline = require('readline-sync');
-let name = readline.question('Add meg a játék nevét.');
-let descr = readline.question('Add meg a játék leírását.');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root:dmArtini1994@localhost/zeta_test');
 // console.log(sequelize);
-let str = 'insert into games (name, description) values("' + name + '", "' + descr + '");';
 
-sequelize
-  .authenticate()
-  .then(() => {
-    sequelize.query(str).then(result => {
-      sequelize.query('select * from games;').then(result => {
-        let n = result[0];
-        let matrix = [];
-        for (let i = 0; i < n.length; i++) {
-          matrix.push([
-            n[i].id,
-            n[i].name,
-            n[i].description
-          ]);
-        }
-        console.log(table(matrix));
+let newData;
+newData = readline.question('Szeretnél új adatot felvenni? Válaszolj Igen/Nem szavakkal.');
+switch (newData) {
+  case 'Igen':
+    let name = readline.question('Add meg a játék nevét.');
+    let descr = readline.question('Add meg a játék leírását.');
+    let str = 'insert into games (name, description) values("' + name + '", "' + descr + '");';
+    sequelize
+      .authenticate()
+      .then(() => {
+        sequelize.query(str).then(result => {
+          sequelize.query('select * from games;').then(result => {
+            let n = result[0];
+            let matrix = [];
+            for (let i = 0; i < n.length; i++) {
+              matrix.push([
+                n[i].id,
+                n[i].name,
+                n[i].description
+              ]);
+            }
+            console.log(table(matrix));
+          });
+        });
+      })
+      .catch(err => {
+        console.error('Unable to connect to the database:', err);
       });
-    });
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    break;
+  case 'Nem':
+    sequelize
+      .authenticate()
+      .then(() => {
+        sequelize.query('select * from games;').then(result => {
+          let n = result[0];
+          let matrix = [];
+          for (let i = 0; i < n.length; i++) {
+            matrix.push([
+              n[i].id,
+              n[i].name,
+              n[i].description
+            ]);
+          }
+          console.log(table(matrix));
+        });
+      })
+      .catch(err => {
+        console.error('Unable to connect to the database:', err);
+      });
+    break;
+  default:
+    console.log('Helytelen választás. A program kilép.');
+    process.exit();
+}
